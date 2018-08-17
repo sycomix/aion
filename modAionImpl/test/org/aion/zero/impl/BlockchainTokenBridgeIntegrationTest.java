34065dde02963a9acf79f3e66c2d96f48f891113
qoire <mr.sun.yao@gmail.com>
2018-08-17 12:18:45 -0400

    token balance transfer tests

--- modAionImpl/test/org/aion/zero/impl/BlockchainTokenBridgeIntegrationTest.java
+++ modAionImpl/test/org/aion/zero/impl/BlockchainTokenBridgeIntegrationTest.java
@@ -1,9 +1,12 @@
 package org.aion.zero.impl;
 
+import org.aion.base.type.Address;
 import org.aion.base.util.ByteUtil;
 import org.aion.crypto.ECKey;
 import org.aion.crypto.ECKeyFac;
 import org.aion.mcf.core.ImportResult;
+import org.aion.mcf.vm.types.DataWord;
+import org.aion.mcf.vm.types.DoubleDataWord;
 import org.aion.precompiled.ContractFactory;
 import org.aion.precompiled.encoding.AbiEncoder;
 import org.aion.zero.impl.types.AionBlock;
@@ -63,11 +66,66 @@ public class BlockchainTokenBridgeIntegrationTest {
         }
     }
 
+    @Test
+    public void testDepositCoinsToContractOwner() {
+        // anyone should be able to send money to the contract
+        StandaloneBlockchain.Bundle bundle = new StandaloneBlockchain.Builder()
+                .withDefaultAccounts(Arrays.asList(TEST_OWNER_EC, ECKeyFac.inst().create()))
+                .withValidatorConfiguration("simple")
+                .build();
+
+        AionTransaction tx = bridgeCall(ByteUtil.EMPTY_BYTE_ARRAY, BigInteger.ZERO, BigInteger.ONE);
+        tx.sign(TEST_OWNER_EC);
+
+        StandaloneBlockchain bc = bundle.bc;
+        AionBlock block = bc.createNewBlock(bc.getBestBlock(), Collections.singletonList(tx), true);
+        assertThat(block.getTransactionsList().size()).isEqualTo(1);
+
+        ImportResult result = bc.tryToConnect(block);
+        assertThat(result.isSuccessful());
+
+        // check balances
+        assertThat(bc.getRepository().getBalance(ContractFactory.getATBContractAddress())).isEqualTo(BigInteger.ONE);
+
+        // check that the contract has been initialized
+        assertThat(bc.getRepository().getStorageValue(ContractFactory.getATBContractAddress(), DataWord.ZERO))
+                .isEqualTo(new DoubleDataWord(TEST_OWNER_EC.getAddress()));
+    }
+
+    @Test
+    public void testDepositCoinsNonOwner() {
+        // anyone should be able to send money to the contract
+        StandaloneBlockchain.Bundle bundle = new StandaloneBlockchain.Builder()
+                .withDefaultAccounts(Arrays.asList(TEST_OWNER_EC, ECKeyFac.inst().create()))
+                .withValidatorConfiguration("simple")
+                .build();
+
+        AionTransaction tx = bridgeCall(ByteUtil.EMPTY_BYTE_ARRAY, BigInteger.ZERO, BigInteger.ONE);
+        tx.sign(bundle.privateKeys.get(1));
+
+        StandaloneBlockchain bc = bundle.bc;
+        AionBlock block = bc.createNewBlock(bc.getBestBlock(), Collections.singletonList(tx), true);
+        assertThat(block.getTransactionsList().size()).isEqualTo(1);
+
+        ImportResult result = bc.tryToConnect(block);
+        assertThat(result.isSuccessful());
+
+        // check balances
+        assertThat(bc.getRepository().getBalance(ContractFactory.getATBContractAddress())).isEqualTo(BigInteger.ONE);
+
+        // check that the contract has been initialized
+        assertThat(bc.getRepository().getStorageValue(ContractFactory.getATBContractAddress(), DataWord.ZERO)).isNull();
+    }
+
     private static AionTransaction bridgeCall(byte[] payload, BigInteger nonce) {
+        return bridgeCall(payload, nonce, BigInteger.ZERO);
+    }
+
+    private static AionTransaction bridgeCall(byte[] payload, BigInteger nonce, BigInteger value) {
         return new AionTransaction(
                 nonce.toByteArray(),
                 ContractFactory.getATBContractAddress(),
-                BigInteger.ZERO.toByteArray(),
+                value.toByteArray(),
                 payload,
                 100000L,
                 BigInteger.TEN.pow(10).longValueExact());
