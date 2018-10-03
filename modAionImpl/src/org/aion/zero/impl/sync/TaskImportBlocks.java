47a3fe3296609587bfde92d2e8ffeb5c0eed5af4
Alexandra <alexandra@nuco.io>
2018-10-03 16:46:35 -0400

    task for storing blocks

--- modAionImpl/src/org/aion/zero/impl/sync/TaskImportBlocks.java
+++ modAionImpl/src/org/aion/zero/impl/sync/TaskImportBlocks.java
@@ -1,32 +1,25 @@
 /*
  * Copyright (c) 2017-2018 Aion foundation.
  *
- * This file is part of the aion network project.
+ *     This file is part of the aion network project.
  *
- * The aion network project is free software: you can redistribute it
- * and/or modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation, either version 3 of
- * the License, or any later version.
+ *     The aion network project is free software: you can redistribute it
+ *     and/or modify it under the terms of the GNU General Public License
+ *     as published by the Free Software Foundation, either version 3 of
+ *     the License, or any later version.
  *
- * The aion network project is distributed in the hope that it will
- * be useful, but WITHOUT ANY WARRANTY; without even the implied
- * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details.
+ *     The aion network project is distributed in the hope that it will
+ *     be useful, but WITHOUT ANY WARRANTY; without even the implied
+ *     warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *     See the GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with the aion network project source files.
- * If not, see <https://www.gnu.org/licenses/>.
+ *     You should have received a copy of the GNU General Public License
+ *     along with the aion network project source files.
+ *     If not, see <https://www.gnu.org/licenses/>.
  *
- * The aion network project leverages useful source code from other
- * open source projects. We greatly appreciate the effort that was
- * invested in these projects and we thank the individual contributors
- * for their work. For provenance information and contributors
- * please see <https://github.com/aionnetwork/aion/wiki/Contributors>.
- *
- * Contributors to the aion source files in decreasing order of code volume:
- * Aion foundation.
+ * Contributors:
+ *     Aion foundation.
  */
-
 package org.aion.zero.impl.sync;
 
 import static org.aion.p2p.P2pConstant.COEFFICIENT_NORMAL_PEERS;
@@ -48,6 +41,8 @@ import java.util.Objects;
 import java.util.SortedSet;
 import java.util.TreeSet;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.stream.Collectors;
 import org.aion.base.util.ByteArrayWrapper;
@@ -104,6 +99,9 @@ final class TaskImportBlocks implements Runnable {
         this.state = new PeerState(NORMAL, 0L);
     }
 
+    ExecutorService executors =
+            Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+
     @Override
     public void run() {
         Thread.currentThread().setPriority(Thread.MAX_PRIORITY);
@@ -156,6 +154,7 @@ final class TaskImportBlocks implements Runnable {
                             + Thread.currentThread().getName()
                             + "] performing block imports was shutdown.");
         }
+        executors.shutdown();
     }
 
     /**
@@ -293,13 +292,12 @@ final class TaskImportBlocks implements Runnable {
 
                 // if any block results in NO_PARENT, all subsequent blocks will too
                 if (importResult == ImportResult.NO_PARENT) {
-                    int stored = chain.storePendingBlockRange(batch);
+                    executors.submit(new TaskStorePendingBlocks(chain, batch, displayId, log));
 
                     if (log.isDebugEnabled()) {
                         log.debug(
                                 "Stopped importing batch due to NO_PARENT result. "
-                                        + "Stored {} out of {} blocks starting at hash = {}, number = {} from node = {}.",
-                                stored,
+                                        + "Batch of {} blocks starting at hash = {}, number = {} from node = {} delegated to storage.",
                                 batch.size(),
                                 b.getShortHash(),
                                 b.getNumber(),
@@ -321,18 +319,7 @@ final class TaskImportBlocks implements Runnable {
                             }
                         case LIGHTNING:
                             {
-                                if (stored < batch.size()) {
-                                    state =
-                                            attemptLightningJump(
-                                                    getBestBlockNumber(),
-                                                    state,
-                                                    peerStates.values(),
-                                                    baseList,
-                                                    chain);
-
-                                } else {
-                                    state.setBase(b.getNumber() + batch.size());
-                                }
+                                state.setBase(b.getNumber() + batch.size());
                                 break;
                             }
                         case THUNDER:
